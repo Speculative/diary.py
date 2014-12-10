85df4b797670d29afeca2ca7f6f5dcea9019c708
Jeffrey Tao <jat2164@columbia.edu>
2014-12-10 18:06:26 -0500

    Basic ConfigParse stuff

--- diary.py
+++ diary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import argparse
+import ConfigParser
 import sys
 import os
 import datetime
@@ -7,7 +8,11 @@ import time
 import subprocess
 
 DIARY_DIR = os.path.expanduser("~/.diary/")
-EDITOR = "vim"
+CONFIG_FILE = ".diary.conf"
+CONFIG = None
+DEFAULT_CONFIG = {
+    "editor": "vim"
+}
 
 def get_args():
     parser = argparse.ArgumentParser(description="Command line diary")
@@ -15,17 +20,32 @@ def get_args():
     return parser.parse_args(sys.argv[1:])
 
 def get_config():
-    return
+    parser = ConfigParser.SafeConfigParser()
+    config_path = os.path.join(DIARY_DIR, CONFIG_FILE)
+    if os.path.isfile(config_path):
+        parser.read(config_path)
+        return parser._sections["Diary"]
+    else:
+        return None
 
 def default_entry_name():
     return str(datetime.date.fromtimestamp(int(time.time())))
 
 def edit_entry(entry_name = default_entry_name()):
+    global CONFIG
+    print type(CONFIG)
+    print CONFIG
     entry_path = os.path.join(DIARY_DIR, entry_name)
-    subprocess.call([EDITOR, entry_path])
+    if ("editor" in CONFIG):
+        subprocess.call([CONFIG["editor"], entry_path])
+    else:
+        subprocess.call([DEFAULT_CONFIG["editor"], entry_path])
 
 def main():
+    global CONFIG
+
     args = get_args()
+    CONFIG = get_config()
 
     if not os.path.isdir(DIARY_DIR):
         os.mkdir(DIARY_DIR)
