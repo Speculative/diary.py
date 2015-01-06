791675bb8c6a401fc5aff81f75dc7e7f48f97d01
Jeffrey Tao <jat2164@columbia.edu>
2015-01-06 01:00:59 -0500

    Added stuff for more convenient entry naming
    You can now do things like
    	diary -v yesterday
    	diary -e january 1

--- diary.py
+++ diary.py
@@ -6,6 +6,7 @@ import os
 import datetime
 import time
 import subprocess
+import re
 
 DIARY_DIR = os.path.expanduser("~/.diary/")
 CONFIG_FILE = ".diary.conf"
@@ -14,13 +15,17 @@ DEFAULT_CONFIG = {
     "editor": "vim"
 }
 
+MONTHS = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY",
+    "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"]
+
 def get_args():
     parser = argparse.ArgumentParser(description="Command line diary")
     parser.add_argument("-l", "--list", action="store_true", help="List entries")
-    parser.add_argument("-e", "--edit", action="store", nargs="?",
-        const=default_entry_name(), help="--edit [entry]")
-    parser.add_argument("-v", "--view", action="store", nargs="?",
-        const=default_entry_name(), help="--view [entry]")
+    parser.add_argument("-e", "--edit", action="store", nargs="+",
+        help="--edit [entry]")
+    parser.add_argument("-v", "--view", action="store", nargs="+",
+        help="--view [entry]")
+    parser.add_argument("-b", "--backup", action="store_true", help="Run backup")
     return parser.parse_args(sys.argv[1:])
 
 def get_config():
@@ -33,12 +38,14 @@ def get_config():
         return None
 
 def default_entry_name():
-    return str(datetime.date.fromtimestamp(int(time.time())))
+    return str(datetime.datetime.now().date())
 
 def edit_entry(entry_name = default_entry_name()):
     global CONFIG
 
+    entry_name = parse_entry_name(entry_name)
     entry_path = os.path.join(DIARY_DIR, entry_name)
+
     if ("editor" in CONFIG):
         subprocess.call([CONFIG["editor"], entry_path])
     else:
@@ -52,15 +59,58 @@ def list_entries():
         print entry
 
 def view_entry(entry_name = default_entry_name()):
+    entry_name = parse_entry_name(entry_name)
     entry_path = os.path.join(DIARY_DIR, entry_name)
+
     if not os.path.isfile(entry_path):
         print "No entry for " + entry_name
         return
+
     with open(entry_path) as f:
         line = f.readline()
         while line:
-            print line
+            print line.rstrip()
             line = f.readline()
+
+def parse_entry_name(entry_name):
+    # Assumes something of the form January 1 2015
+    # or 2015-01-01.
+    global MONTHS
+    global CONFIG
+
+    SPECIAL_ENTRY_NAMES = ["TODAY", "YESTERDAY", "TOMORROW"]
+
+    if isinstance(entry_name, list):
+        entry_name = " ".join(entry_name)
+
+    if re.match(r"\d{4}-\d{2}-\d{2}", entry_name):
+        return entry_name
+    elif entry_name.upper() in SPECIAL_ENTRY_NAMES:
+        if entry_name.upper() == "TODAY":
+            return default_entry_name()
+        elif entry_name.upper() == "YESTERDAY":
+            return str(datetime.datetime.now().date() -
+                datetime.timedelta(days=1))
+        elif entry_name.upper() == "TOMORROW":
+            return str(datetime.datetime.now().date() +
+                datetime.timedelta(days=1))
+    else:
+        if entry_name.count(" ") == 1:
+            year = datetime.date.today().year
+            month, day = entry_name.split(" ")
+            month = month.upper()
+            month = MONTHS.index(month) + 1
+            return "%04d-%02d-%02d" % (year, month, int(day))
+        elif entry_name.count(" ") == 2:
+            month, day, year = entry_name.split(" ")
+            month = month.upper()
+            month = MONTHS.index(month) + 1
+            return "%04d-%02d-%02d" % (year, month, int(day))
+        else:
+            print "Unrecognized date format!"
+
+def backup():
+    print "You tried to run a backup!"
 
 def main():
     global CONFIG
@@ -73,6 +123,8 @@ def main():
 
     if args.list:
         list_entries()
+    elif args.backup:
+        backup()
     elif args.view:
         view_entry(args.view)
     elif args.edit:
